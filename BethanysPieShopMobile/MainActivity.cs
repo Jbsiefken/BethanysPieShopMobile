48ac392ddb73ce721ee827e5f3d0b77e0fe7189f
Josh Siefken <joshbsiefken@gmail.com>
2020-07-04 17:25:07 -0500

    Adds a button to main activity

--- BethanysPieShopMobile/MainActivity.cs
+++ BethanysPieShopMobile/MainActivity.cs
@@ -15,7 +15,20 @@ namespace BethanysPieShopMobile
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+            //Views must be identified by their id in Resource.designer.cs
+            //this is because views and activities aren't formally associated until runtime
+            Button myButton = FindViewById<Button>(Resource.Id.MyButton);
+            //on click of myButton, call method MyButton_Click
+            myButton.Click += MyButton_Click;
         }
+
+        private void MyButton_Click(object sender, System.EventArgs e)
+        {
+            //a static Toast method for displaying a message
+            var toast = Toast.MakeText(this, "A button was clicked", ToastLength.Short);
+            toast.Show();
+        }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
